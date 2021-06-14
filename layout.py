13fd3a15e05346e41fd429edbd652ba60c51579d
Alonso <hguzman.sistemas@gmail.com>
2021-06-14 12:30:51 -0500

    Julia call removed

--- layout.py
+++ layout.py
@@ -255,7 +255,7 @@ def get_layout(number_categories, no_solutions, budget, buckets, table_d, table_
     ## Start the DOM layout
     layout = dbc.Container([
         navbar,
-        asked_no_sols_card,
+        #asked_no_sols_card,
         spinner,
         header,        
         main_div,
